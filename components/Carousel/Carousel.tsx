6562a446831b1ea6e50814333838355e9fef31cd
Lloyd Polley <lloydpolley@googlemail.com>
2024-04-09 11:39:18 +0100

    widget design

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -7,44 +7,21 @@ import TrackSquare from "../Tracks/TrackSquare";
 import { getAllArtists } from "../../api/getTracks";
 import { getUserFollowers } from "../../api/addFollower";
 import Link from "next/link";
+import UserWidget from "../UserWidget";
 
 const cx = classNames.bind(style);
 
 const Carousel = ({ users, searchParams, hp }) => {
-  // const [users, setUsers] = useState([]);
-  // const [loading, setLoading] = useState(false);
-
-  // // console.log("searchParams", searchParams);
-  // // console.log("params", params);
-
-  // const getArtists = async () => {
-  //   setLoading(true);
-  //   const artists = await getUserFollowers(params?.id);
-  //   console.log("getArtists", artists);
-  //   setUsers(artists);
-  //   setLoading(false);
-  // };
-
   if (searchParams?.f !== "following" && !hp) {
     return null;
   }
 
   return (
-    <div className={cx("carousel-wrapper")}>
-      <div className={cx("carousel")}>
+    <div className={cx("carousel")}>
+      <div className={cx("carousel__inner")}>
         {users &&
           users?.slice(0, 11).map((user) => {
-            return (
-              <div className={cx("carousel__container")} key={user?.uid}>
-                <Link
-                  className={cx("carousel__element")}
-                  href={`/${user?.uid}?f=all`}
-                >
-                  <img src={user.profile} />
-                </Link>
-                <p>{user.displayName}</p>
-              </div>
-            );
+            return <UserWidget user={user} key={user?.uid} />;
           })}
       </div>
     </div>
---
+++ components/UserWidget/UserWidget.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import classNames from "classnames/bind";
+import style from "./UserWidget.module.scss";
+import Link from "next/link";
+
+const cx = classNames.bind(style);
+
+const UserWidget = ({ user }) => {
+  return (
+    <div className={cx("user")} key={user?.uid}>
+      <Link className={cx("user__element")} href={`/${user?.uid}?f=all`}>
+        <img src={user.profile} />
+      </Link>
+      <p>{user.displayName}</p>
+    </div>
+  );
+};
+
+export default UserWidget;
