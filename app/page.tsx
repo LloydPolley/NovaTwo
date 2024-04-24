5ca4a100ed4eb5f915fd8511dd503d12a8059b57
Lloyd Polley <lloydpolley@googlemail.com>
2024-04-25 00:53:59 +0100

    tweaks to padding 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,14 @@
 import classNames from "classnames/bind";
 import styles from "./Home.module.scss";
-import Wrapper from "../components/Wrapper";
-import TrackContainerArtist from "../components/Tracks/TrackContainer/TrackContainerArtist";
-import FilterBar from "../components/FilterBar";
+
 import Carousel from "../components/Carousel";
 import { getAllArtists } from "../api/getTracks";
 import Hero from "../components/Hero";
-import {
-  getAllTracksOrdered,
-  getArtistTracks,
-  getTracksWhere,
-} from "../api/getTracks";
+import { getTracksWhere } from "../api/getTracks";
 import Track from "../components/Tracks/Track";
 import UserWidget from "../components/UserWidget";
-import { Suspense } from "react";
 
 const cx = classNames.bind(styles);
-
-const filters = [
-  { label: "All", url: "/" },
-  { label: "Tracks", url: "?f=tracks" },
-  { label: "Mix", url: "?f=mix" },
-];
 
 export default async function Dj({ searchParams, params }) {
   const users = await getAllArtists();
