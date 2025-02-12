50f610f75632e7aff10094504eec3afef0c380ac
Lloyd Polley <lloydpolley@googlemail.com>
2025-02-12 01:00:08 +0000

    style missing

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,9 @@
-import classNames from "classnames/bind";
-import styles from "./Page.module.scss";
 import AuthWidget from "../../components/Forms/AuthWidget";
-import ProfileForm from "../../components/Forms/ProfileForm";
 import { Suspense } from "react";
-
-const cx = classNames.bind(styles);
 
 function SignInScreen() {
   return (
-    <div className={cx("page")}>
+    <div>
       <Suspense>
         <AuthWidget />
       </Suspense>
