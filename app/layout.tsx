ee86ac7baa0cfaa98933f9bf86d67c644751468e
Lloyd Polley <lloydpolley@googlemail.com>
2025-02-12 01:45:30 +0000

    hydrations suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,6 @@ export default function RootLayout(props) {
               <Wrapper>{children}</Wrapper>
             </div>
           </Suspense>
-
           <AudioWidget />
         </GlobalProvider>
         <Analytics />
