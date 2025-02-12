f0e090ae8bfa5e6a61df25a45aee453e3bae8c0b
Lloyd Polley <lloydpolley@googlemail.com>
2025-02-12 01:37:38 +0000

    BE key unpublic

--- utils/firebaseAdmin.ts
+++ utils/firebaseAdmin.ts
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase-admin/firestore";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(
-      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+      JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_SERVICE_ACCOUNT)
     ),
   });
 }
