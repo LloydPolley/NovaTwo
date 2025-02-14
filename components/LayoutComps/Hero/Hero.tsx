4ec6b1768f78e47b8eda7b92a26ea7e758789ab9
Lloyd Polley <lloydpolley@googlemail.com>
2025-02-14 16:52:39 +0000

    hero add

--- components/LayoutComps/Hero/Hero.tsx
+++ components/LayoutComps/Hero/Hero.tsx
@@ -1,22 +1,30 @@
 "use client";
 
-import classNames from "classnames/bind";
-import style from "./Hero.module.scss";
 import Link from "next/link";
-
-const cx = classNames.bind(style);
 
 const Hero = () => {
   return (
-    <Link href={"/bxD1nSsSGHecXzH6tQWUCRSlGok1"}>
-      <div className={cx("hero")}>
-        <div className={cx("hero__content")}>
-          <div className={cx("hero__text")}>
-            <h1>End of Genesys</h1>
-          </div>
+    <div
+      className="relative h-[325px] rounded-3xl overflow-hidden mb-5 bg-cover bg-center bg-no-repeat lg:p-4"
+      style={{
+        backgroundImage: 'url("/release1.jpg")',
+        backgroundPosition: "center 40%",
+      }}
+    >
+      <div className="flex flex-col justify-around h-full">
+        <h1 className="text-white text-center font-bold text-4xl mt-[75px] lg:mt-[50px]">
+          Create, share, <br /> connect
+        </h1>
+        <div className="flex justify-center gap-4">
+          <Link className="" href="/login">
+            Get started
+          </Link>
+          <Link className="" href="/discover?f=releases">
+            Discover
+          </Link>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
