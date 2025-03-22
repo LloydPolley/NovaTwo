d7ae81a0c5568e67d7513037b2967c77f0f1ac6c
Lloyd Polley <lloydpolley@googlemail.com>
2025-03-22 02:05:16 +0000

    remove ts restrictions

--- components/Music/TrackContainer/TrackContainer.tsx
+++ components/Music/TrackContainer/TrackContainer.tsx
@@ -4,11 +4,7 @@ import Track from "../Track";
 import { TrackType } from "../../../types/tracks";
 import { ReleaseType } from "@/types/releases";
 
-type TracksProps = {
-  tracks?: (TrackType | ReleaseType)[];
-};
-
-const TrackContainer = ({ tracks }: TracksProps) => {
+const TrackContainer = ({ tracks }) => {
   console.log("tracks", tracks);
   return (
     <div className="grid gap-5 w-full p-5 flex-grow flex-wrap grid-cols-2 md:grid-cols-4 xl:grid-cols-5">
